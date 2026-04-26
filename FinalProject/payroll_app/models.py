848323a33df51e888730dabd50b8d9293c19156c
elyshaleanneviray-cpu <elysha.leanne.viray@student.ateneo.edu>
2026-04-26 21:49:22 +0800

    set up/added files

--- FinalProject/payroll_app/models.py
+++ FinalProject/payroll_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# Create your models here.
+class Employee(models.Model):
+
+    id = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    rate = models.FloatField
+    overtime = models.FloatField
+    allowance = models.FloatField
+    
+    def getName(self):
+        return self.name
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
