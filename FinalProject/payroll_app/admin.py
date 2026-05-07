6f6ec2536414e017f3ade0b505e181352de4a69f
Calista <calista.caparas@gmail.com>
2026-05-07 14:06:03 +0800

    registered classes in admin.py

--- FinalProject/payroll_app/admin.py
+++ FinalProject/payroll_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Account, Employee, Payslip
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Employee)
+admin.site.register(Payslip)
